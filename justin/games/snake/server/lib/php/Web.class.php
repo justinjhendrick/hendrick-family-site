ac057782e32e8baa26ed5328cf7b35a5b732821d
Justin Hendrick <justinjhendrick@gmail.com>
2016-12-02 18:17:43 -0700

    trying for hi score by putting server code in another dir

---
+++ justin/games/snake/server/lib/php/Web.class.php
@@ -0,0 +1,53 @@
+<?php
+
+class php_Web {
+	public function __construct(){}
+	static function getParams() {
+		$a = array_merge($_GET, $_POST);
+		if(get_magic_quotes_gpc()) {
+			reset($a); while(list($k, $v) = each($a)) $a[$k] = stripslashes((string)$v);
+		}
+		return php_Lib::hashOfAssociativeArray($a);
+	}
+	static function getClientHeader($k) {
+		$k1 = null;
+		{
+			$s = strtoupper($k);
+			$k1 = str_replace("-", "_", $s);
+		}
+		if(null == php_Web::getClientHeaders()) throw new HException('null iterable');
+		$__hx__it = php_Web::getClientHeaders()->iterator();
+		while($__hx__it->hasNext()) {
+			unset($i);
+			$i = $__hx__it->next();
+			if($i->header === $k1) {
+				return $i->value;
+			}
+		}
+		return null;
+	}
+	static $_client_headers;
+	static function getClientHeaders() {
+		if(php_Web::$_client_headers === null) {
+			php_Web::$_client_headers = new HList();
+			$h = php_Lib::hashOfAssociativeArray($_SERVER);
+			if(null == $h) throw new HException('null iterable');
+			$__hx__it = $h->keys();
+			while($__hx__it->hasNext()) {
+				unset($k);
+				$k = $__hx__it->next();
+				if(_hx_substr($k, 0, 5) === "HTTP_") {
+					php_Web::$_client_headers->add(_hx_anonymous(array("header" => _hx_substr($k, 5, null), "value" => $h->get($k))));
+				} else {
+					if(_hx_substr($k, 0, 8) === "CONTENT_") {
+						php_Web::$_client_headers->add(_hx_anonymous(array("header" => $k, "value" => $h->get($k))));
+					}
+				}
+			}
+		}
+		return php_Web::$_client_headers;
+	}
+	static $isModNeko;
+	function __toString() { return 'php.Web'; }
+}
+php_Web::$isModNeko = !php_Lib::isCli();
