d9116de09a443c4a03b94532d8ba7b7a96536f87
Justin Hendrick <justinjhendrick@gmail.com>
2016-12-02 17:47:39 -0700

    trying to connect server and client for hi score

--- justin/games/snake/Snake.js
+++ justin/games/snake/Snake.js
@@ -1400,7 +1400,7 @@ var Client = function() { };
 $hxClasses["Client"] = Client;
 Client.__name__ = ["Client"];
 Client.main = function(score,name) {
-	var cnx = haxe_remoting_HttpAsyncConnection.urlConnect("/justin/games/snake");
+	var cnx = haxe_remoting_HttpAsyncConnection.urlConnect("http://www.hendrick.family/justin/games/snake");
 	cnx.setErrorHandler(function(err) {
 		haxe_Log.trace("Error: " + err,{ fileName : "Client.hx", lineNumber : 4, className : "Client", methodName : "main"});
 	});
@@ -4247,7 +4247,7 @@ var lime_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 309567;
+	this.version = 808600;
 };
 $hxClasses["lime.AssetCache"] = lime_AssetCache;
 lime_AssetCache.__name__ = ["lime","AssetCache"];
